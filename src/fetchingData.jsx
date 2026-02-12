6012e0ef370833fdbe7b649e532c74637518cb13
Khatri <Kamal.Khatri@seamk.fi>
2026-02-12 11:48:27 +0200

    latest changes

--- src/fetchingData.jsx
+++ src/fetchingData.jsx
@@ -1,44 +1,36 @@
 import axios from 'axios'
 import { useState, useEffect } from 'react'
 
-
 const FetchingDataComponent = () => {
 
-    const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([])
 
+  useEffect(() => {
+    console.log('fetching data...')
 
-    useEffect(() => {
-        console.log('fetching data...')
+    axios
+      .get('http://localhost:3001/api/persons')
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
 
-        axios
-            .get('http://localhost:3001/api/persons')
-            .then(response => {
-                console.log(response.data)
-                setPersons(response.data)
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error)
-            })
+  }, [])
 
-    }, [])
-
-    return (
-        <>
-
-            <ul className='listStyle'>
-                {persons.map((person, i) => (
-                    <li key={i} >
-                        <p>{person.id}</p>
-                        <p>{person.content}</p>
-                        <p>{person.important}</p>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
-
+  return (
+    <ul className='listStyle'>
+      {persons.map(person => (
+        <li key={person.id}>
+          <p>ID: {person.id}</p>
+          <p>Name: {person.name}</p>
+          <p>Number: {person.number}</p>
+        </li>
+      ))}
+    </ul>
+  )
 }
-
-
 
 export default FetchingDataComponent
